7b40bc21ec84f5fd430d28e5d6fbd472e3e329b5
苏昊远 <1109069349@qq.com>
2024-06-04 22:52:38 +0800

    firstGd

---
+++ GDSStudy/Script/firstGd.gd
@@ -0,0 +1,16 @@
+extends Node2D;
+
+func _init():
+	print("init");
+	pass;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("ready");
+	pass; # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print("process");
+	pass;
