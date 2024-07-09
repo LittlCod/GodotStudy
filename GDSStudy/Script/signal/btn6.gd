21beedd72b56c816f40504f9b7a74682e3e27d51
苏昊远 <1109069349@qq.com>
2024-07-09 17:24:06 +0800

    多线程

--- GDSStudy/Script/signal/btn6.gd
+++ GDSStudy/Script/signal/btn6.gd
@@ -8,8 +8,8 @@ func _ready():
 	$"../Button".connect("pressed", Callable(self, "_on_button1_pressed"))
 	# 绑定给Button3的pressed信号，触发Button2的_on_click_Btn2方法
 	$"../Button3".connect("pressed", Callable($"../Button2", "_on_click_Btn2"))
- 	# 绑定给Button4的mySignal信号，触发自身的_on_button4_pressed方法
-	$"../Button4".connect("mySignal", Callable(self, "_on_button4_pressed"))
+ 	# 绑定给Button4的mySignal信号，触发自身的_on_button4_pressed方法，自身的方法不需要写Callable函数
+	$"../Button4".connect("mySignal", _on_button4_pressed)
 
 func _on_button1_pressed():
 	print("button1 被点击了")
--- GDSStudy/Script/thread/moreThread.gd
+++ GDSStudy/Script/thread/moreThread.gd
@@ -6,17 +6,12 @@ func _ready():
 func _onButton():
 	var myThread = Thread.new()
 	myThread.start(_textThread.bind('这', '是'))
-	print('线程id：', myThread.get_id())
-	print('线程活跃：', myThread.is_alive())
-	
-	var waitForThread = myThread.wait_to_finish()
-	
-	print('线程结果：', waitForThread)
-	print('结束线程id：', myThread.get_id())
-	print('结束线程活跃：', myThread.is_alive())
+	var wait = await myThread.wait_to_finish()
+	print(wait)
 	
 func _textThread(a, b):
 	print(a, b, 'thread')
+	await get_tree().create_timer(5).timeout
 	return b+a
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
