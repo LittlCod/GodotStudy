1ab73f125e2b41bac674ff1065a68d8e8b06f513
苏昊远 <1109069349@qq.com>
2024-07-09 08:59:18 +0800

    信号

---
+++ GDSStudy/Script/signal/btn2.gd
@@ -0,0 +1,12 @@
+extends Node
+
+func _on_click_Btn2():
+	print('点击btn3')
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ GDSStudy/Script/signal/btn4.gd
@@ -0,0 +1,13 @@
+extends Node
+
+#自定义信号
+signal mySignal(a, b)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
