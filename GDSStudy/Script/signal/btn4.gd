ade28508a25f72fcc45cb2d776a4af31d38d551d
苏昊远 <1109069349@qq.com>
2024-07-09 16:49:41 +0800

    自定义信号

--- GDSStudy/Script/signal/btn4.gd
+++ GDSStudy/Script/signal/btn4.gd
@@ -3,9 +3,12 @@ extends Node
 #自定义信号
 signal mySignal(a, b)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.connect("pressed", Callable(self, "handleMySignal"))
+	
+func handleMySignal():
+	print(123)
+	emit_signal("mySignal", '参数1', '参数2耶耶耶')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- GDSStudy/Script/signal/btn6.gd
+++ GDSStudy/Script/signal/btn6.gd
@@ -9,14 +9,15 @@ func _ready():
 	# 绑定给Button3的pressed信号，触发Button2的_on_click_Btn2方法
 	$"../Button3".connect("pressed", Callable($"../Button2", "_on_click_Btn2"))
  	# 绑定给Button4的mySignal信号，触发自身的_on_button4_pressed方法
-	$"../Button4".connect("mySignal", Callable(self, "_on_Button4_pressed"))
+	$"../Button4".connect("mySignal", Callable(self, "_on_button4_pressed"))
 
 func _on_button1_pressed():
 	print("button1 被点击了")
 	
-func _on_button4_pressed():
-	print("button4 被点击了")
+func _on_button4_pressed(a, b):
+	print("button4 被点击了", a, b)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
